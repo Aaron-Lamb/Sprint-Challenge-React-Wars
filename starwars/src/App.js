f57e6bb6e49d4c85d941fee564d1af466ee97e65
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-01 17:16:24 -0700

    Ditched the pages idea, ran into a cors issue and changed to the rick and morty api, with one state and one useeffect. Set my characterState to the response.data.results.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,38 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import axios from "ax"
+import axios from "axios";
 
 const App = () => {
-  // I noticed that there are a total of nine pages of characters, and 
-  // decided to write state for each of them, as I have no immediate
-  // thoughts on how to do it without this.
+  // I will set my state here.
+  const [characterState, setCharacterState] = useState([]);
 
-  // Page one of state
-  const [charactersOne, setCharactersOne] = ([]);
-
-  // Page Two of state
-  const [ charactersTwo, setCharactersTwo] = ([]);
-
-  // Page three of state
-  const [ charactersThree, setCharactersThree] = ([]);
-
-  // Page four
-  const [ charactersFour, setCharactersFour] = ([]);
-
-  // Page five
-  const [ charactersFive, setCharactersFive] = ([]);
-
-  // Page six
-  const [ charactersSix, setCharactersSix] = ([]);
-
-  // Page seven
-  const [ charactersSeven, setCharactersSeven] = ([]);
-
-  // Page eight
-  const [ charactersEight, setCharactersEight] = ([]); 
-
-  // Page nine
-  const [ charactersNine, setCharactersNine] = ([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -40,13 +13,17 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  // I will also create a subsequent 9 useffects for each page in the API
-
-  // Page One
+  // Now setting my state to the r&m api
   useEffect(() => {
-
+    axios.get("https://rickandmortyapi.com/api/character")
+    .then(response => {
+      setCharacterState(response.data.results)
+    })
+    .catch(error => {
+      console.log(error)
+    })
   }, [])
-
+console.log(characterState);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>

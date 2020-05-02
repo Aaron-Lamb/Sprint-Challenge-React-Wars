9e6f795ed0c630560f184109e3bd62183791444b
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-01 17:28:02 -0700

    Returning a div with a key of props.id, an h1 with props.name, and subsequent h4 with props.status, species, type, gender, and location.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,11 +1,16 @@
 // Write your Character component here
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-component";
 
 const CharacterCreator = (props) => {
     return(
         <div key={props.id}>
-
+            <h1>{props.name}</h1>
+            <h4>{props.status}</h4>
+            <h4>{props.species}</h4>
+            <h4>{props.type}</h4>
+            <h4>{props.gender}</h4>
+            <h4>{props.location}</h4>
         </div>
     )
 }
